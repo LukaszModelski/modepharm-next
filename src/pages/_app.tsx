f2fda341087ad941a21909c9711c0aface7d0e85
Czapa <lukaszmodelski90@gmail.com>
2024-05-03 22:07:40 +0200

    removed context, cleand type app type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/app'
-import { ModepharmType } from '../helpers/zod'
 import { Layout } from '@/layouts/Layout'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '@/styles/theme'
@@ -8,11 +7,7 @@ import { Source_Sans_3 } from 'next/font/google'
 
 const sourceSans3 = Source_Sans_3({ subsets: ['latin'] })
 
-type AppCustomProps = {
-  modepharm?: ModepharmType
-}
-
-export default function MyApp({ Component, pageProps, modepharm }: AppProps & AppCustomProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
