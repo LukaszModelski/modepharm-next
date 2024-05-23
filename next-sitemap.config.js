e662de3a69b324b7e5f1df9fc053f4ce52c498d7
Czapa <lukaszmodelski90@gmail.com>
2024-05-24 00:27:07 +0200

    postbuild script for generating sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.modepharm.pl/',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  outDir: 'out'
+}
